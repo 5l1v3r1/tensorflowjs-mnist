f090e3990c8db994606c658202314ac5dc74c34c
Junya Ishihara <junya@champierre.com>
2019-08-21 18:31:17 +0900

    Extract functions got from tensorflow.js tutorial to lib.js.

--- script.js
+++ script.js
@@ -1,205 +1,81 @@
 import {MnistData} from './data.js';
+import {showExamples, getModel, train} from './lib.js'
 
-let drawElement;
-let model;
-let signaturePad;
+const IMAGE_WIDTH = 28;
+const IMAGE_HEIGHT = 28;
 
+// 手書きパネルを初期化
 function initSignaturePad() {
-  drawElement = document.getElementById('draw-area');
-  signaturePad = new SignaturePad(drawElement, {
-     minWidth: 6,
-     maxWidth: 6,
+  let pad = document.getElementById('pad');
+  return new SignaturePad(pad, {
+     minWidth: 5,
+     maxWidth: 5,
      penColor: 'white',
      backgroundColor: 'black',
   });
 }
 
-async function showExamples(data) {
-  // Create a container in the visor
-  const surface =
-    tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data'});
+// 「認識」ボタンが押されたときの処理
+function predict(model) {
+  // canvasを280x280から28x28にリサイズ
+  let ctx = document.createElement('canvas').getContext('2d');
+  let pad = document.getElementById('pad');
+  ctx.drawImage(pad, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+  let imageData = ctx.getImageData(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
 
-  // Get the examples
-  const examples = data.nextTestBatch(20);
-  const numExamples = examples.xs.shape[0];
+  // 認識結果を導く
+  let score = tf.tidy(() => {
+    let input = tf.browser.fromPixels(imageData, 1).reshape([1, IMAGE_WIDTH, IMAGE_HEIGHT, 1]).div(tf.scalar(255));
+    return model.predict(input);
+  });
 
-  // Create a canvas element to render each example
-  for (let i = 0; i < numExamples; i++) {
-    const imageTensor = tf.tidy(() => {
-      // Reshape the image to 28x28 px
-      return examples.xs
-        .slice([i, 0], [1, examples.xs.shape[1]])
-        .reshape([28, 28, 1]);
-    });
+  // 確率が最も高かった数字
+  let maxConfidence = score.dataSync().indexOf(Math.max.apply(null, score.dataSync()));
 
-    const canvas = document.createElement('canvas');
-    canvas.width = 28;
-    canvas.height = 28;
-    canvas.style = 'margin: 4px;';
-
-    await tf.browser.toPixels(imageTensor, canvas);
-    surface.drawArea.appendChild(canvas);
-
-    let imageData = canvas.getContext('2d').getImageData(0, 0, 28, 28);
-
-    imageTensor.dispose();
-  }
+  // 認識結果を出力する
+  let elements = document.querySelectorAll(".confidence");
+  elements.forEach((el, i) => {
+    el.parentNode.classList.remove('is-selected');
+    if (i === maxConfidence) {
+      el.parentNode.classList.add('is-selected');
+    }
+    el.innerText = score.dataSync()[i];
+  })
 }
 
-function getModel() {
-  model = tf.sequential();
-
-  const IMAGE_WIDTH = 28;
-  const IMAGE_HEIGHT = 28;
-  const IMAGE_CHANNELS = 1;
-
-  // In the first layer of our convolutional neural network we have
-  // to specify the input shape. Then we specify some parameters for
-  // the convolution operation that takes place in this layer.
-  model.add(tf.layers.conv2d({
-    inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-    kernelSize: 5,
-    filters: 8,
-    strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'varianceScaling'
-  }));
-
-  // The MaxPooling layer acts as a sort of downsampling using max values
-  // in a region instead of averaging.
-  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-
-  // Repeat another conv2d + maxPooling stack.
-  // Note that we have more filters in the convolution.
-  model.add(tf.layers.conv2d({
-    kernelSize: 5,
-    filters: 16,
-    strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'varianceScaling'
-  }));
-  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-
-  // Now we flatten the output from the 2D filters into a 1D vector to prepare
-  // it for input into our last layer. This is common practice when feeding
-  // higher dimensional data to a final classification output layer.
-  model.add(tf.layers.flatten());
-
-  // Our last layer is a dense layer which has 10 output units, one for each
-  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
-  const NUM_OUTPUT_CLASSES = 10;
-  model.add(tf.layers.dense({
-    units: NUM_OUTPUT_CLASSES,
-    kernelInitializer: 'varianceScaling',
-    activation: 'softmax'
-  }));
-
-  // Choose an optimizer, loss function and accuracy metric,
-  // then compile and return the model
-  const optimizer = tf.train.adam();
-  model.compile({
-    optimizer: optimizer,
-    loss: 'categoricalCrossentropy',
-    metrics: ['accuracy'],
-  });
-
-  return model;
-}
-
-async function train(model, data) {
-  const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
-  const container = {
-    name: 'Model Training', styles: { height: '1000px' }
-  };
-  const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
-
-  const BATCH_SIZE = 512;
-  const TRAIN_DATA_SIZE = 5500;
-  const TEST_DATA_SIZE = 1000;
-
-  const [trainXs, trainYs] = tf.tidy(() => {
-    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
-    return [
-      d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
-      d.labels
-    ];
-  });
-
-  const [testXs, testYs] = tf.tidy(() => {
-    const d = data.nextTestBatch(TEST_DATA_SIZE);
-    return [
-      d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
-      d.labels
-    ];
-  });
-
-  return model.fit(trainXs, trainYs, {
-    batchSize: BATCH_SIZE,
-    validationData: [testXs, testYs],
-    epochs: 10,
-    shuffle: true,
-    callbacks: fitCallbacks
-  });
-}
-
-function reset() {
+// 「リセット」ボタンが押されたときの処理
+function reset(signaturePad) {
   signaturePad.clear();
-  let elements = document.querySelectorAll(".accuracy");
+  let elements = document.querySelectorAll(".confidence");
   elements.forEach(el => {
     el.parentNode.classList.remove('is-selected');
     el.innerText = '-';
   })
 }
 
-function predict() {
-  const inputWidth = 28;
-  const inputHeight = 28;
-
-  // resize
-  let ctx = document.createElement('canvas').getContext('2d');
-  ctx.drawImage(drawElement, 0, 0, inputWidth, inputHeight);
-  let imageData = ctx.getImageData(0, 0, inputWidth, inputHeight);
-
-  let score = tf.tidy(() => {
-    let input = tf.browser.fromPixels(imageData, 1).reshape([1, 28, 28, 1]).cast('float32').div(tf.scalar(255));
-    return model.predict(input);
-  });
-
-  let maxAccuracy = score.dataSync().indexOf(Math.max.apply(null, score.dataSync()));
-  let elements = document.querySelectorAll(".confidence");
-  let i = 0;
-  elements.forEach(el => {
-    el.parentNode.classList.remove('is-selected');
-    if (maxAccuracy === i) {
-      el.parentNode.classList.add('is-selected');
-    }
-    el.innerText = score.dataSync()[i];
-    i++;
-  })
-}
-
 async function run() {
-  initSignaturePad();
-
-  document.getElementById("predict-button").onclick = function() {
-    predict();
-  };
-
-  document.getElementById("reset-button").onclick = function() {
-    reset();
-  };
+  let signaturePad = initSignaturePad();
 
   const data = new MnistData();
   await data.load();
   await showExamples(data);
 
-  const model = getModel();
+  let model = getModel();
   tfvis.show.modelSummary({name: 'Model Architecture'}, model);
 
   await train(model, data);
 
+  // 学習モデルの作成が終わったら、ローディングメッセージを非表示にし、手書きパネルを表示
   document.getElementById("loading-message").style.display = "none"
   document.getElementById("main").style.display = "block"
+
+  document.getElementById("predict-button").onclick = function() {
+    predict(model);
+  };
+
+  document.getElementById("reset-button").onclick = function() {
+    reset(signaturePad);
+  };
 }
 
 document.addEventListener('DOMContentLoaded', run);
